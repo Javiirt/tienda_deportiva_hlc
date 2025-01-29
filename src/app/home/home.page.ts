f4a1424837b99432c8198a2399e8c9e7a3b78426
Javiirt <javivirodri3702@gmail.com>
2025-01-29 13:42:35 +0100

    Primera subida a github

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Producto } from '../producto';
+import { FirestoreService } from '../firestore.service';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +10,29 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  productoEditando: Producto ;
+
+  constructor(private firestoreService: FirestoreService) {
+    //Crear un producto vacío al empezar
+    this.productoEditando ={} as Producto;
+
+  }
+
+  clicBotonInsertar() {
+    if (this.productoEditando && Object.keys(this.productoEditando).length > 0) {
+      this.firestoreService.insertar('productos', this.productoEditando).then(
+        () => {
+          console.log('Producto insertado correctamente');
+          this.productoEditando = {} as Producto;
+        },
+        (error) => {
+          console.error('Error al insertar producto', error);
+        }
+      );
+    } else {
+      console.error('Producto inválido: No se pueden insertar datos vacíos.');
+    }
+  }
+  
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyCDd9dRHffKpSZXTZ9kwuFIp2XdJZk_4-o",
+    authDomain: "proyecto-tienda-deportiva.firebaseapp.com",
+    projectId: "proyecto-tienda-deportiva",
+    storageBucket: "proyecto-tienda-deportiva.firebasestorage.app",
+    messagingSenderId: "566209539995",
+    appId: "1:566209539995:web:0d1317cd14b5d8f68bbb3e"
+  }
 };
 
 /*
